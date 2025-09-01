b4c1c83ba7b0564ea8b28244290b367f8d029a5b
abdul-rehman-ops <abdul.rehman@conradlabs.com>
2025-09-01 19:25:07 +0500

    change in delivery

--- utils/deliveryZones.js
+++ utils/deliveryZones.js
@@ -2,12 +2,12 @@
 export const DELIVERY_ZONES = [
   { maxDistance: 7, charge: 100, eta: "60–90 minutes" },
   { maxDistance: 11, charge: 150, eta: "60–90 minutes" },
-  { maxDistance: 17, charge: 250, eta: "90–120 minutes" },
-  { maxDistance: 20, charge: 300, eta: "120–135 minutes" },
-  { maxDistance: 23, charge: 350, eta: "135–150 minutes" },
-  { maxDistance: 26, charge: 400, eta: "150–165 minutes" },
-  { maxDistance: 29, charge: 450, eta: "165–180 minutes" },
-  { maxDistance: 31, charge: 500, eta: "165–180 minutes" },
+  { maxDistance: 14, charge: 200, eta: "90–120 minutes" },
+  { maxDistance: 19, charge: 250, eta: "90–120 minutes" },
+  { maxDistance: 22, charge: 300, eta: "120–135 minutes" },
+  { maxDistance: 25, charge: 350, eta: "135–150 minutes" },
+  { maxDistance: 28, charge: 400, eta: "150–165 minutes" },
+  { maxDistance: 30, charge: 450, eta: "165–180 minutes" },
 ];
 
 export const getDeliveryZoneByDistance = (distance) => {
